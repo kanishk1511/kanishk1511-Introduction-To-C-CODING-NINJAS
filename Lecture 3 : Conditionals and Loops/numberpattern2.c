795eca9d5e297b1021161d4ff8615542928f2a8c
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 11:43:54 +0530

    Create numberpattern2.c

---
+++ Lecture 3 : Conditionals and Loops/numberpattern2.c
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	
+	cin >> n;
+    int i =1;
+    while(i<=n) {
+        int k =1;
+        while( k <= n-i ){
+            cout <<" ";
+            k++;
+
+        }
+        int j=1;
+        int val=i;
+        while(j<=i){
+            cout << val;
+            j++;
+            val++;
+
+        }
+        cout << endl;
+        i++;
+
+    }
+}
+    
