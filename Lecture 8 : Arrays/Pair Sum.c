77c6b2f0e0fe208f259173db8c0681684a31739c
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 12:26:00 +0530

    Create Pair Sum.c

---
+++ Lecture 8 : Arrays/Pair Sum.c
@@ -0,0 +1,20 @@
+
+int pairSum(int *input, int size, int x)
+{
+	int count = 0;
+	for(int i = 0 ; i < size ; i++){
+		for( int j = i + 1 ; j < size ; j++){
+			if(input[i] + input[j] == x){
+				count++;
+
+			}
+		}
+		
+		
+		
+
+	}
+	return count;
+
+	//Write your code here
+}
