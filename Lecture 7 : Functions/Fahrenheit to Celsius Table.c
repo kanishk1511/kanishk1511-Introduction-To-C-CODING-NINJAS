3039319d33ddc17fc18f09df5735f42a886c5876
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 12:13:57 +0530

    Create Fahrenheit to Celsius Table.c

---
+++ Lecture 7 : Functions/Fahrenheit to Celsius Table.c
@@ -0,0 +1,13 @@
+void printTable(int start, int end, int step) {
+        while(start <= end){
+            int k = (5*(start-32)/9) ;
+        cout << start << "\t" << k << endl;
+            start += step;
+        }
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Print output and don't return it.
+     * Taking input is handled automatically.
+     */
+    
+}
