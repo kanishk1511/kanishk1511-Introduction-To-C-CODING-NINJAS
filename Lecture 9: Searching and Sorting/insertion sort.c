492f1a4abc7b2198c7faea16b4252291abf05039
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 12:31:14 +0530

    Create insertion sort.c

---
+++ Lecture 9: Searching and Sorting/insertion sort.c
@@ -0,0 +1,16 @@
+void insertionSort(int *arr, int n)
+{
+    for(int i = 0 ; i < n ; i++){
+        int current = arr[i];
+        int j;
+        for( j = i - 1 ; j >= 0 ; j--){
+            if(current < arr[j]){
+                arr[j + 1] = arr[j];
+            }else{
+                break;
+            }
+        }
+        arr[j + 1] = current;
+    }
+    //Write your code here
+}
