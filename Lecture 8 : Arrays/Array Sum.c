026af6def663eb4aaa81255b3d621a7624c72750
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 12:15:46 +0530

    Create Array Sum.c

---
+++ Lecture 8 : Arrays/Array Sum.c
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int input[n];
+	
+	for(int i = 0; i < n; i++) {
+		cin >> input[i];
+	}
+
+	int sum = 0;
+
+	for(int i = 0; i < n; i++) {
+			sum += input[i];
+		
+	}
+
+	cout << sum << endl;
+
+}
